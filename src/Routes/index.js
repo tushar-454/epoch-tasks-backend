0769ba48fb936fa4d1405af01cbe1f362c57a8f1
tushar-454 <imtushar454@gmail.com>
2023-12-22 12:20:07 +0600

    jwt token create and remove api added

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const taskRoutes = require('./task');
+const jwtRoutes = require('./jwt');
 
 router.use('/api/v1/task', taskRoutes);
+router.use('/api/v1/jwt', jwtRoutes);
 
 module.exports = router;
